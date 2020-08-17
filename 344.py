65306ad59848ed52f760d805f7b93b6d85f835a2
urmilshah1 <51014316+urmilshah1@users.noreply.github.com>
2020-08-17 12:29:16 -0700

    344
    One of the easier problems on leetcode. Simply use the reverse function to reverse the order of the list and it should work.

---
+++ 344.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+        
+''' one of the more easier questions, there were a couple of ways i could think of doing this. The conditions were in place reverse string.
+Hence simply using the reverse function built in python works just fine'''
