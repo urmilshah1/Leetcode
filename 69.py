709caba31a7663a43ab7660a24df659f934ee750
Urmil Premal Shah <51014316+urmilshah1@users.noreply.github.com>
2020-08-18 12:01:01 -0700

    69
    This is a simple sqrt function used to take the square root of the number.

---
+++ 69.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        x1 = int(sqrt(x))
+        return x1
+    
+    #simply take the square root of the number and type cast it using a int data type.
