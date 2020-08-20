d372054fcd0b302d65faada7b9bd916c038f1a6d
Urmil Premal Shah <51014316+urmilshah1@users.noreply.github.com>
2020-08-20 11:03:13 -0700

    Delete 1480.py

--- 1480.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        sums = []  #make an array 
-        a = 0  #Intialize a to 0 
-        for i in nums:
-            a += i   #Increment a by the number at index i 
-            sums.append(a) #add it to the list
-        return sums #return the entire list
