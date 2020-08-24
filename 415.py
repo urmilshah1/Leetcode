ac04f41ebd62a4f51586d2788361c31b29e8335b
Urmil Premal Shah <51014316+urmilshah1@users.noreply.github.com>
2020-08-23 19:49:20 -0700

    Add Strings
    Add two numbers and convert them as a string. Return the string

---
+++ 415.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        sum1 = 0
+        sum1 = int(num1) + int(num2)
+        sum2 = str(sum1)
+        return sum2
+    
+    
+    #add two numbers and convert as a string .Return the string
