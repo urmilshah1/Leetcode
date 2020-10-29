514848aa7f34d4ac66b2c2bc99573343c643cff5
Urmil Premal Shah <51014316+urmilshah1@users.noreply.github.com>
2020-10-29 12:00:47 -0700

    Update 66.py

--- 66.py
+++ 66.py
@@ -1,6 +1,12 @@
 class Solution:
-    List = []
     def plusOne(self, digits: List[int]):
-        for i in digits:
-            print(i, end="")
+        n = len(digits)
+        for i in range(n):
+            j = n -1 -i
+            if digits[j] == 9:
+                digits[j] = 0    
+            else: 
+                digits[j] += 1
+                return digits
+        return [1] + digits
         
